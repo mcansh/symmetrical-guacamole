7818c6519f5ddf49c58b2331b8bc4165f7d0cd7c
Logan McAnsh <logan@mcan.sh>
2019-12-18 16:03:35 -0500

    chore: add styled-components
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- App.tsx
+++ App.tsx
@@ -1,19 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Text, View } from "react-native";
+import styled from "styled-components/native";
+
+const MyView = styled.View`
+  flex: 1;
+  background-color: black;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MyText = styled.Text`
+  color: white;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <MyView>
+      <MyText>Open up App.tsx to start working on your app!</MyText>
+    </MyView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
