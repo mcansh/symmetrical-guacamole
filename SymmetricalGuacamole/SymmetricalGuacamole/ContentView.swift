4aee6eeb48120f416ce10c8a3affa13b09474e11
Logan McAnsh <logan@mcan.sh>
2019-12-18 22:32:20 -0500

    chore: add blank xcode project
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ SymmetricalGuacamole/SymmetricalGuacamole/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SymmetricalGuacamole
+//
+//  Created by Logan McAnsh on 12/18/19.
+//  Copyright © 2019 Logan McAnsh. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
