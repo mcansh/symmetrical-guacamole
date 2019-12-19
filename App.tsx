d3eff483f7c95a758419dd0a7ec4d62b81dbbe76
Logan McAnsh <logan@mcan.sh>
2019-12-18 22:28:50 -0500

    chore(deps): add eslint, prettier
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- App.tsx
+++ App.tsx
@@ -1,21 +1,59 @@
-import React from "react";
-import styled from "styled-components/native";
+import React from 'react';
+import styled from 'styled-components/native';
+import {
+  StatusBar,
+  Text,
+  StyleSheet,
+  Button,
+  NativeModules,
+  View,
+} from 'react-native';
+import { iOSUIKit, iOSColors } from 'react-native-typography';
 
-const View = styled.View`
+const MyView = styled.View`
   flex: 1;
   background-color: black;
   align-items: center;
   justify-content: center;
+  flex: 1;
+  justify-content: center;
 `;
 
-const Text = styled.Text`
-  color: white;
-`;
+const styles = StyleSheet.create({
+  yellowTitle: {
+    color: iOSColors.yellow,
+  },
+});
 
-export default function App() {
+const App = () => {
+  const getMusic = async () => {
+    console.log(NativeModules);
+  };
+
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <MyView>
+      <StatusBar barStyle="light-content" />
+      <View>
+        <Text style={[iOSUIKit.body, styles.yellowTitle]}>
+          Open up App.tsx to start working on your app!
+        </Text>
+        <View
+          style={{
+            padding: 10,
+            backgroundColor: iOSColors.yellow,
+            borderRadius: 8,
+            marginTop: 20,
+          }}
+        >
+          <Button
+            onPress={getMusic}
+            color={iOSColors.white}
+            title="Get Music"
+          />
+        </View>
+      </View>
+    </MyView>
   );
-}
+};
+
+export default App;
